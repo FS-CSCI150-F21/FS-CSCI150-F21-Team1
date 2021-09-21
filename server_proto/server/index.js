5eec8ca7dc4a7fd46f702abc606e4c735dc39bb5
Aran <48372721+ashayer@users.noreply.github.com>
2021-09-21 02:15:14 -0700

    Add files via upload

--- server_proto/server/index.js
+++ server_proto/server/index.js
@@ -1,1 +1,34 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
 
+app.use(cors());    
+app.use(express.json());
+
+
+app.post("/employees", async(req,res) =>{
+    try {
+        const {employee_name} = req.body;
+        const newEmployee = await pool.query(
+            "INSERT INTO employee_info (employee_name) VALUES($1) RETURNING *",[employee_name]
+        );
+
+        res.json(newEmployee.rows[0]);
+    } catch (error) {
+        console.error(error.message)
+    }
+});
+
+app.get("/employees", async(req, res) => {
+    try {
+        const allEmployees = await pool.query("SELECT * FROM employee_info");
+        res.json(allEmployees.rows);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+app.listen(5000, () =>{
+    
+})
