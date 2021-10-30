f043034961ca56e9e978aa20ddbb972c470c8bac
Michael Nugent <NugentMichael@live.com>
2021-10-29 23:58:26 -0700

    Menu presentation improved. Category view started. Will need menu.sql eventually to initialize others' databases.

---
+++ rest_man_proto/SQL/menu.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE rest_info;
+
+-- need to write script that initializes tables and data
+
+
+-- need to create user and assign privileges
+-- $dbusername = "rest_manager";
+-- $dbpassword = "iF2ONNbmcCTcdjrd";
+-- $dbname = "rest_info";
--- rest_man_proto/php_pages/menu.php
+++ rest_man_proto/php_pages/menu.php
@@ -1,18 +1,18 @@
 <?php
-//echo 'test';
-//echo "test";
+
 $servername = "127.0.0.1";
 $dbusername = "rest_manager";
 $dbpassword = "iF2ONNbmcCTcdjrd";
 $dbname = "rest_info";
-//echo 'tee';
+
 $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
-//echo 'hey';
+
 if ($conn->connect_error) {
     echo 'not connected to mysql server';
     exit('Could not connect');
-} 
+}
 /*
+//debug
 else {
     echo $servername, ' mySQL server connected with username: ', $dbusername, ' and 
         dbname: ', $dbname;
@@ -20,21 +20,65 @@ else {
 echo '<br>', $conn->host_info;
 */
 
-$query = "SELECT * FROM menuCategories;";
+//will be decided based on type of data needed, as described by $_GET['request']
 
-$qry_result = $conn->query($query);
+if ($_GET['request'] == 'cats') {
+    $query = "SELECT * FROM menuCategories;";
 
-//in case database table name is changed or empty
-$exists = $qry_result->num_rows;
-if (!$exists) {
-    echo 'No categories.  Database table menuCategories is empty, or renamed.';
+    $qry_result = $conn->query($query);
+
+    //in case database table name is changed or empty
+    $exists = $qry_result->num_rows;
+    if (!$exists) {
+        echo 'No categories.  Database table ' . $_GET['request'] . 'is empty, or renamed.';
+    }
+
+    $records = $qry_result->fetch_all();
+
+    $jsonStr = json_encode($records);
+    echo $jsonStr;
+
+} else {
+    //$_GET['request'] should be an integer to represent the selected category.
+    //use it directly in query.
+    $subcategoriesQuery = "SELECT * FROM menuSubcategories WHERE category=" . $_GET['request'];
+    $qry_result = $conn->query($subcategoriesQuery);
+
+    //array of records from menuSubcategories table where category=$_GET['request']
+    /*
+    $subcategoryRecs = $qry_result->fetch_object();
+    echo $subcategoryRecs->name;
+    $subcategoryRecs = $qry_result->fetch_object();
+    echo $subcategoryRecs->name;
+    */
+    $subcategoryRecs = $qry_result->fetch_all(MYSQLI_BOTH);
+    echo $subcategoryRecs[0]["name"];
+    echo $subcategoryRecs[1][0];
+    /*
+    foreach($subcategoryRecs as $rec){
+        echo $rec["name"];
+    }
+    */
+
+
+    $itemsQuery = "SELECT * FROM menuItems WHERE category=".$_GET['request'].' ORDER BY subcategory ASC';
+
+
+
+    /*debug
+    $jsonStr = json_encode($subcategoryRecs);
+    echo $jsonStr;
+    */
+
+    //$query;
 }
 
-//debug stuff
-//echo $qry_result->num_rows;
+$conn->close();
 
-$categories = $qry_result->fetch_all();
-//echo $categories[0][0];
+//debug
+//$query = $query . '0';
+//echo $query;
+
 
 //debug stuff
 /*
@@ -44,8 +88,3 @@ foreach ($drinks as $val) {
     echo $val;
 }
 */
-
-$jsonStr = json_encode($categories);
-echo $jsonStr;
-
-//echo $qry_result->available;
