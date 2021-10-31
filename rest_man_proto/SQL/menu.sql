98676e7b04feb2bb742375ebc87759a9d613ea4f
Michael Nugent <NugentMichael@live.com>
2021-10-30 23:41:15 -0700

    sql
    Saved Aran and Long's respective users, and people counter sql files shared in discord.  Added my own for menu.

--- rest_man_proto/SQL/menu.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE rest_info;
-
--- need to write script that initializes tables and data
-
-
--- script should create user and assign privileges
--- $dbusername = "rest_manager";
--- $dbpassword = "iF2ONNbmcCTcdjrd";
--- $dbname = "rest_info";
