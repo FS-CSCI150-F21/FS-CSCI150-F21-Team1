3c64671d300a4a323fb2c711a982ae011febef11
Michael Nugent <NugentMichael@live.com>
2021-10-30 17:22:53 -0700

    Exported menu.html's js and css to external files. Cleaned up codes and added comments to menu related files.

---
+++ rest_man_proto/js_pages/menu.js
@@ -0,0 +1,207 @@
+/*
+menu.js requests information from menu.php in order to present appropriate
+food items on menu.html
+*/
+
+
+//used on load to show the main categories of the menu.
+//this data is grabbed from the mysql database.  see menu.php file.
+function categories() {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let responseObj = JSON.parse(httpRequest.responseText);
+            let catBod = document.getElementById('catTblBod');
+            for (let i = 0; i < responseObj.length; i++) {
+                //only populate category if marked as "available"
+                if (responseObj[i][2] == true) {
+
+                    //table row
+                    let tr = document.createElement('tr');
+                    tr.onclick = function () { firstCategoryChange(i) };
+
+                    //table row cell
+                    let td = document.createElement('td');
+                    td.innerText = responseObj[i][0];
+                    td.className = 'catName';
+
+                    /*
+                    //may not need "odd vs even" anymore.  was for css.
+                    colorClass = (i % 2) ? "odd" : "even";
+                    td.className = 'catName + ' + colorClass;
+                    */
+
+                    //add category name table cell to table row element
+                    tr.appendChild(td);
+
+                    //new table cell element to house image.
+                    td = document.createElement('td');
+                    td.className = 'imgCell';
+                    let img = document.createElement('img');
+                    img.src = "../images/menu/" + responseObj[i][3];
+                    img.width = "350";
+                    img.height = "300";
+
+                    //smaller sizes like these could be used for menuEditor
+                    //img.width = "50";
+                    //img.height = "50";
+
+                    //append image to table cell, table cell to table row,
+                    //and table row to table body.
+                    td.appendChild(img);
+                    tr.appendChild(td);
+                    catBod.appendChild(tr);
+                }
+            }
+        }
+    }
+    httpRequest.open("GET", "../php_pages/menu.php?request=cats", true);
+    httpRequest.send();
+}
+
+//called by any category (a table row) of the initial page layout.
+//never used afterwards
+//loads items of clicked category
+function firstCategoryChange(i) {
+
+    let nav = document.createElement('nav');
+    let catNames = document.getElementsByClassName('catName');
+
+    let ul = document.createElement('ul');
+    for (let i = 0; i < catNames.length; i++) {
+        let li = document.createElement('li');
+        li.innerText = catNames[i].innerText;
+        li.onclick = function () { catChange(i) };
+        ul.appendChild(li);
+    }
+
+    //alert(ul.children[i].innerText);
+    ul.children[i].id = 'selectedNavItem';
+    let selectedCategoryText = ul.children[i].innerText;
+    document.getElementById('h1Title').innerText = selectedCategoryText;
+    document.getElementById('title').innerText = 'Menu: ' + selectedCategoryText;
+
+    //append unordered list to navigation bar, and nav bar to document body
+    nav.appendChild(ul)
+    document.body.appendChild(nav);
+
+    //remove categories table
+    document.getElementById('catTbl').remove();
+
+    //access database through php page to get selected category's information
+    let httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        alert('Cannot create an XMLHTTP instance');
+        return false;
+    }
+    httpRequest.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            if (httpRequest.responseText) {
+                categoryDisplay(httpRequest.responseText, i);
+            }
+
+        }
+    }
+    httpRequest.open("GET", "../php_pages/menu.php?request=" + i, true);
+    httpRequest.send();
+}
+function catChange(i) {
+    let subcatTbls = document.getElementsByClassName('subcategory');
+    while (subcatTbls[0]) {
+        subcatTbls[0].remove();
+    }
+
+    //might package this in another function.  redundant.
+    let httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        alert('Cannot create XMLHttpRequest instance');
+        return false;
+    }
+    httpRequest.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            categoryDisplay(httpRequest.responseText, i);
+        }
+    }
+    httpRequest.open("GET", "../php_pages/menu.php?request=" + i);
+    httpRequest.send();
+}
+
+function categoryDisplay(jsonStr, catIndex) {
+
+    //remove previous category's information, if it exists.
+    let subcatTbls = document.getElementsByClassName('subcategory');
+    while (subcatTbls[0]) {
+        subcatTbls[0].remove();
+    }
+
+    //array of 2-element objects: "name":"<subcategory>" and "items":<array>
+    let subcatsArr = JSON.parse(jsonStr);
+
+    //go through each subcategory
+    for (let i = 0; i < subcatsArr.length; i++) {
+
+        //object of array
+        //"subcategory" and "items"
+        let subcat = subcatsArr[i];
+
+        //use table's caption element as title
+        let tbl = document.createElement('table');
+        tbl.className = 'subcategory';
+        let caption = document.createElement('caption');
+        caption.innerText = subcat["name"];
+        tbl.appendChild(caption);
+
+
+        //thead contains column headers.  will be hidden in css.
+        let thead = document.createElement('thead');
+        let tr = document.createElement('tr');
+        let th = document.createElement('th');
+        th.innerText = "Name";
+        tr.appendChild(th);
+        th = document.createElement('th');
+        th.innerText = "Description";
+        tr.appendChild(th);  //consider id cell, too
+        th = document.createElement('th');
+        th.innerText = "Price";
+        tr.appendChild(th);
+        thead.appendChild(tr);
+        tbl.appendChild(thead);
+
+
+        //array of items from subcategory
+        let items = subcat["items"];
+
+        let tbody = document.createElement('tbody');
+        //fill tbody with items
+        for (let j = 0; j < items.length; j++) {
+            //let itemTbl = document.createElement('table');
+            tr = document.createElement('tr');
+
+            //item name
+            let td = document.createElement('td');
+            td.innerText = items[j]['name'];
+            td.className = "itemName";
+            tr.appendChild(td);
+
+            //item description
+            td = document.createElement('td');
+            td.innerText = items[j]['description'];
+            td.className = 'itemDescription';
+            tr.appendChild(td);
+
+            //item price
+            td = document.createElement('td');
+            td.innerText = items[j]['price'];
+            td.className = 'itemPrice';
+            tr.appendChild(td);
+
+            tbody.appendChild(tr);
+        }
+
+        //add this subcategory's table to document
+        tbl.appendChild(tbody);
+        document.body.appendChild(tbl);
+
+        //rinse & repeat
+    }
+}
--- rest_man_proto/php_pages/menu.php
+++ rest_man_proto/php_pages/menu.php
@@ -1,50 +1,110 @@
 <?php
 
+//mySQL server authentication details
 $servername = "127.0.0.1";
 $dbusername = "rest_manager";
 $dbpassword = "iF2ONNbmcCTcdjrd";
 $dbname = "rest_info";
 
+//establish connection
 $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 
+//verify connection
 if ($conn->connect_error) {
     echo 'not connected to mysql server';
     exit('Could not connect');
 }
 
-
+//helps group items with their appropriate parent subcategory for JSON
 class subcategory implements JsonSerializable
 {
-    public $name; //change these all to private once json_serialization implemented
-    public $id;
-    public $items = array();
+    private $name;
+    private $id;
+    private $items = array();
     function __construct($recObj)
     {
         $this->name = $recObj->name;
-        //echo 'Created subcategory object: ';
-        //echo $recObj->name . '<br>';
         $this->id = $recObj->id;
-        //echo 'name: ' . $this->name;
-        /*
-        foreach($this->items as $ele){
-            echo $ele;
-        }
-        */
     }
+
     public function addItem($recObj)
     {
-        //echo 'addItem Function <br>';
         //verify rec id has same id as this id.
-        //only reason this would fail is if mysql was wrong
+        //not sure why this would ever fail, but adds security at server's expense
         if ($this->id == $recObj->subcategory) $this->items[] = $recObj;
         else return false;
         return true;
     }
     public function jsonSerialize()
     {
-        return ['name' => $this->name, 'items'=>$this->items];
+        return ['name' => $this->name, 'items' => $this->items];
     }
 }
+
+//type of data queried determined by $_GET['request']
+if ($_GET['request'] == 'cats') {
+    //first call to php fills the menu page with the main categories.
+
+    //sql query as a string
+    $query = "SELECT * FROM menuCategories;";
+
+    //execute query on mySQL server
+    $qry_result = $conn->query($query);
+
+    //verify results are not empty in case: table, user, or database were changed
+    $exists = $qry_result->num_rows;
+    if (!$exists) {
+        echo 'No categories.  Database table ' . $_GET['request'] 
+            . 'is empty, or renamed.';
+    }
+
+    //store results in php's memory.
+    //numerically indexed array, as opposed to associative array, or objects.
+    $records = $qry_result->fetch_all();
+
+    //stringify records array
+    $jsonStr = json_encode($records);
+
+    //return results to client
+    echo $jsonStr;
+} else {
+    //any call after first to this page returns a specific subcategories items
+
+    //$_GET['request'] should be an integer to represent the selected category.
+    $subcategoriesQuery = "SELECT name, id FROM menuSubcategories 
+        WHERE category=" . $_GET['request'] . ' AND available=1';
+
+    //make subcategories query on mySQL database
+    $qry_result = $conn->query($subcategoriesQuery);
+
+    //records as objects from menuSubcategories table for client-requested subcategory
+    while ($rec = $qry_result->fetch_object()) {
+        $subcategoryObjs[] = new subcategory($rec);
+    }
+
+
+    //query to retrieve items
+    $itemsQuery = "SELECT name, id, description, price, subcategory
+        FROM menuItems WHERE category=" . $_GET['request'] . ' ORDER BY subcategory ASC';
+
+    //make query and store result.
+    $itemsResult = $conn->query($itemsQuery);
+
+    //adds all items to appropriate subcategory objects through record fetches
+    $i = 0;
+    while ($itemsRec = $itemsResult->fetch_object()) {
+        if (!$subcategoryObjs[$i]->addItem($itemsRec)) {
+            $i++;
+            $subcategoryObjs[$i]->addItem($itemsRec);
+        }
+    }
+
+    //send results to client
+    echo json_encode($subcategoryObjs);
+}
+
+$conn->close();
+
 
 /*
 //debug
@@ -54,35 +114,6 @@ else {
 }
 echo '<br>', $conn->host_info;
 */
-
-//type of data queried determined by $_GET['request']
-if ($_GET['request'] == 'cats') {
-    //first call to php fills the menu page with the main categories.
-    $query = "SELECT * FROM menuCategories;";
-
-    $qry_result = $conn->query($query);
-
-    //in case database table name is changed or empty
-    $exists = $qry_result->num_rows;
-    if (!$exists) {
-        echo 'No categories.  Database table ' . $_GET['request'] . 'is empty, or renamed.';
-    }
-
-    $records = $qry_result->fetch_all();
-
-    $jsonStr = json_encode($records);
-    echo $jsonStr;
-} else {
-    //any call after first to this page returns a specific subcategories items
-
-    //$_GET['request'] should be an integer to represent the selected category.
-    $subcategoriesQuery = "SELECT name, id FROM menuSubcategories 
-        WHERE category=" . $_GET['request'] . ' AND available=1';
-
-    //make query on mySQL database
-    $qry_result = $conn->query($subcategoriesQuery);
-
-    //array of records from menuSubcategories table where category=$_GET['request']
 
     /*
     $subcatsArr = array();
@@ -97,10 +128,8 @@ if ($_GET['request'] == 'cats') {
         $subcatsArr[] = new subcategory($subcategoryRec);
     }
     */
-    while ($rec = $qry_result->fetch_object()) {
-        $subcategoryObjs[] = new subcategory($rec);
-    }
-    //echo json_encode($rec);
+
+ //echo json_encode($rec);
     //echo json_encode($subObj);
     /*
     echo $subcategoryRecs->name;
@@ -148,25 +177,6 @@ if ($_GET['request'] == 'cats') {
     }
     */
 
-
-
-
-    //query to retrieve items
-    $itemsQuery = "SELECT name, id, description, price, subcategory
-        FROM menuItems WHERE category=" . $_GET['request'] . ' ORDER BY subcategory ASC';
-
-    $itemsResult = $conn->query($itemsQuery);
-    $itemsRec = $itemsResult->fetch_object();
-    //echo $itemsRec->name;
-
-    $i = 0;
-    while($itemsRec = $itemsResult->fetch_object()) {
-        if(!$subcategoryObjs[$i]->addItem($itemsRec)) {
-            $i++;
-            $subcategoryObjs[$i]->addItem($itemsRec);
-        }
-    }
-    echo json_encode($subcategoryObjs);
     /*debug
     $jsonStr = json_encode($subcategoryRecs);
     echo $jsonStr;
@@ -181,10 +191,6 @@ if ($_GET['request'] == 'cats') {
 
     echo json_encode($animal,$dog);
     */
-}
-
-$conn->close();
-
 //debug
 //$query = $query . '0';
 //echo $query;
