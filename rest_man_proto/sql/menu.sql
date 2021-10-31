469567df4c2caae23f4ac5590934f15f1c6541ea
Michael Nugent <NugentMichael@live.com>
2021-10-31 09:29:43 -0700

    Update menu.sql
    Tested creation of database, table schemas, records, and user.  Assignment of user privileges might have to be done from root.  Everything worked otherwise.

--- rest_man_proto/sql/menu.sql
+++ rest_man_proto/sql/menu.sql
@@ -1,7 +1,13 @@
---create rest_info database if not already present
+-- script to create rest_info database, rest_manager user that accesses database from php backend, and appropriate menu table schemas and their records.
+
+
+-- create rest_info database if not already present
 CREATE DATABASE IF NOT EXISTS rest_info;
 
+-- create user
 CREATE USER IF NOT EXISTS 'rest_manager'@'localhost' IDENTIFIED BY 'iF2ONNbmcCTcdjrd';
+
+-- grant privileges to newly created user.  this statement is most likely to fail if not ran from root.
 GRANT ALL ON rest_info.* to 'rest_manager'@'localhost';
 
 -- swap to this database
