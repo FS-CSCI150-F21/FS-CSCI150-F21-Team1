ba59c09f393bfdc3cecd58048eddc1faec0fa50f
Long Nguyen <nguyenlonguhs@gmail.com>
2021-11-09 15:49:26 -0800

    updated sql database file

--- rest_man_proto/sql/closed_order_info.sql
+++ rest_man_proto/sql/closed_order_info.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 02, 2021 at 07:52 AM
+-- Generation Time: Nov 10, 2021 at 12:48 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -24,19 +24,42 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order_info`
+-- Table structure for table `closed_order_info`
 --
 
-CREATE TABLE `order_info` (
+CREATE TABLE `closed_order_info` (
+  `order_id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `number` int(10) NOT NULL,
   `togo` tinyint(1) NOT NULL,
+  `people_dining_in` int(11) NOT NULL,
   `status` varchar(50) NOT NULL,
   `created` timestamp NOT NULL DEFAULT current_timestamp(),
   `finished` timestamp NOT NULL DEFAULT current_timestamp(),
   `items` blob NOT NULL,
-  `table_num` int(3) NOT NULL
+  `table_num` int(3) NOT NULL,
+  `prep_time` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `closed_order_info`
+--
+ALTER TABLE `closed_order_info`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `closed_order_info`
+--
+ALTER TABLE `closed_order_info`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
