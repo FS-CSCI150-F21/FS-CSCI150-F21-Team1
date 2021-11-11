c5158415363b8353aa80611c0df06d601cf3f8e1
Michael Nugent <NugentMichael@live.com>
2021-11-11 14:20:43 -0800

    edited incompatible collations

--- rest_man_proto/sql/rest_info.sql
+++ rest_man_proto/sql/rest_info.sql
@@ -103,7 +103,7 @@ CREATE TABLE `menuItems` (
   `name` varchar(127) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `id` int NOT NULL,
   `available` tinyint(1) DEFAULT NULL,
-  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `price` decimal(6,2) DEFAULT NULL,
   `timesOrdered` int DEFAULT NULL,
   `category` int DEFAULT NULL,
@@ -163,9 +163,9 @@ DROP TABLE IF EXISTS `open_order_info`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `open_order_info` (
   `order_id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `dinerTable` int DEFAULT NULL,
-  `status` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `status` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `last_modified` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `finished` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
