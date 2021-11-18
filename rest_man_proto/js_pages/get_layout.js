2c79f893569ccd400a70c2a904693e1f5c25af09
Aran Hayer <ashayer@users.noreply.github.com>
2021-11-17 20:25:33 -0800

    added most of table editing
    still need to add reservations

---
+++ rest_man_proto/js_pages/get_layout.js
@@ -0,0 +1,227 @@
+function set_table(){
+    var j = new XMLHttpRequest();
+    j.onreadystatechange = function () {
+        if (j.readyState == 4 && j.status == 200) {
+            var response = JSON.parse(j.responseText);
+            if(response.grid == null){
+                set_table_coords()
+            }
+            else{
+                //console.log(response.grid);
+                var temprows = response.grid.length;
+                var tempcols = response.grid[0].length;
+
+                table_coords = response.grid;
+                create_grid(table_coords, temprows, tempcols);
+                paintgrid(temprows,tempcols);
+            }
+        }
+    };
+    j.open('POST', '../php_pages/getgrid.php');
+    j.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    j.send();
+
+}
+
+function paintgrid(rows,cols){
+    var newi, newj;
+    for(var i = 0; i < rows; i++){
+        for(var j = 0; j < cols; j++){
+            var current_coord = table_coords[i][j];
+            if(i < 10){
+                newi = "0" + String(i);
+            }
+            else{
+                newi = String(i);
+            }
+            if(j < 10){
+                newj = "0" + String(j);
+            }
+            else{
+                newj = String(j);
+            }
+            var id = newi + newj; 
+            
+            
+            if(isCharacterALetter(current_coord)){
+                letter_cells(current_coord,i,j, id);
+            }
+            else if(current_coord==-1){
+                deleted_cells(current_coord,i,j,id)
+            }
+            else if(current_coord ==0){
+                if(j==0){
+                    document.getElementById(id).style.borderLeft = "2px solid black";
+                }
+                else if(j == table_coords[0].length - 1)
+                {
+                    document.getElementById(id).style.borderRight = "2px solid black";
+                }
+                else if(i ==0){
+                    document.getElementById(id).style.borderTop = "2px solid black";
+                }
+                else if(i == table_coords.length-1){
+                    document.getElementById(id).style.borderBottom = "2px solid black";
+                }
+            }
+
+        }
+
+    }
+}
+function deleted_cells(current_coord,i,j,id){
+    check_delete_above(current_coord,i,j,id);
+    check_delete_below(current_coord,i,j,id);
+    check_delete_left(current_coord,i,j,id);
+    check_delete_right(current_coord,i,j,id);
+}
+function check_delete_above(current_coord,i,j,id){
+    if(i ==0){
+    }
+    else {
+        var up = table_coords[i-1][j];
+        if(isCharacterALetter(up) || (up == 0) || (!up == -1)){
+            
+            document.getElementById(id).style.borderTop = "2px solid black";
+        }
+    }
+}
+function check_delete_below(current_coord,i,j,id){
+    if(i == table_coords.length -1 ){}
+    else {
+        var up = table_coords[i+1][j];
+        if(isCharacterALetter(up) || (up == 0) || (!up == -1)){
+            
+            document.getElementById(id).style.borderBottom = "2px solid black";
+        }
+    }
+}
+function check_delete_left(current_coord,i,j,id){
+    if(j == 0){}
+    else {
+        var up = table_coords[i][j-1];
+        if(isCharacterALetter(up) || (up == 0) || (!up == -1)){
+            document.getElementById(id).style.borderLeft = "2px solid black";
+        }
+    }
+}
+function check_delete_right(current_coord,i,j,id){
+    if(j == table_coords[0].length -1){}
+    else {
+        var up = table_coords[i][j+1];
+        if(isCharacterALetter(up) || (up == 0) || (!up == -1)){
+            document.getElementById(id).style.borderRight = "2px solid black";
+        }
+    }
+}
+function letter_cells(current_coord,i,j,id){
+    check_above(current_coord,i,j,id);
+    check_below(current_coord,i,j,id);
+    check_left(current_coord,i,j,id);
+    check_right(current_coord,i,j,id);
+}
+
+function check_above(current_coord, i, j, id){
+    if(i==0){
+        document.getElementById(id).style.borderTop = "2px solid black";
+    }
+    else{
+        var up = table_coords[i-1][j];
+        console.log(up, current_coord);
+        if((up != current_coord)){
+            document.getElementById(id).style.borderTop = "2px solid black";
+        }
+    }
+
+
+}
+
+function check_below(current_coord, i, j, id){
+    if(i== table_coords.length-1){
+        document.getElementById(id).style.borderBottom = "2px solid black";
+    }
+    else{
+        var down = table_coords[i+1][j];
+        console.log(down, current_coord);
+        if((down != current_coord)){
+            document.getElementById(id).style.borderBottom = "2px solid black";
+        }
+    }
+
+
+}
+
+function check_left(current_coord, i, j, id){
+    if(j==0){
+        document.getElementById(id).style.borderLeft = "2px solid black";
+    }
+    else{
+        var left = table_coords[i][j-1];
+        console.log(current_coord, left);
+        if((left != current_coord)){
+            document.getElementById(id).style.borderLeft = "2px solid black";
+        }
+    }
+
+
+}
+function check_right(current_coord, i, j, id){
+    if(j== table_coords[0].length-1){
+        document.getElementById(id).style.borderRight = "2px solid black";
+    }
+    else{
+        var right = table_coords[i][j+1];
+        console.log(current_coord, right);
+        if((right != current_coord)){
+            document.getElementById(id).style.borderRight = "2px solid black";
+        }
+    }
+
+
+}
+
+function isCharacterALetter(char) {
+  return (/[a-zA-Z]/).test(char)
+}
+
+function create_grid(table_coords,numrows, numcols){
+        var table = document.getElementById("grid");
+        table.innerHTML = "";
+        var newi, newj;
+        for(var i = 0; i < numrows; i++){
+            var row = document.createElement("tr");
+            row.className = "row";
+            row.id = "row" + i;
+            for(var j = 0; j < numcols; j++){
+                var cell = document.createElement("td");
+                cell.className = "box";
+                if(i < 10){
+                    newi = "0" + String(i);
+                }
+                else{
+                    newi = String(i);
+                }
+                if(j < 10){
+                    newj = "0" + String(j);
+                }
+                else{
+                    newj = String(j);
+                }
+                cell.id = newi + newj;
+                if(table_coords[i][j] == 0 || table_coords[i][j] == -1){
+                    cell.innerHTML = "\u00A0";
+                }
+                else{
+                    cell.innerText = table_coords[i][j];
+                }
+                row.appendChild(cell);
+            }
+            table.appendChild(row);
+        }
+}
+
+
+
+
+
+
--- rest_man_proto/js_pages/tableEditor.js
+++ rest_man_proto/js_pages/tableEditor.js
@@ -1,6 +1,7 @@
 var table_coords;
 var global_table_id;
 var newtables = 0;
+var table_objects = [];
 
 function set_table_coords(){
 
@@ -41,11 +42,12 @@ function set_table(){
 
                 document.getElementById("x").value = temprows;
                 document.getElementById("y").value = tempcols;
-
                 table_coords = response.grid;
                 create_grid(table_coords, temprows, tempcols);
                 paintgrid(temprows,tempcols);
                 createtablebuttons(response.numberof);
+                get_all_tables();
+
             }
         }
     };
@@ -67,6 +69,34 @@ function createtablebuttons(numberof){
         parent.appendChild(element);
         newtables++;
     }
+}
+function deletetablebuttons(){
+
+    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    var parent = document.getElementById("table_buttons");
+    var children = parent.children;
+    if(children.length > 0){
+        parent.removeChild(children[children.length - 1]);
+        newtables--;
+    }
+    else
+    {
+        console.log("no more tables");
+    }
+
+    var parent2 = document.getElementById("tables");
+    var children2 = parent2.children;
+    if(children2.length > 1){
+        parent2.removeChild(children2[children2.length - 1]);
+        table_objects.pop();
+    }
+    else
+    {
+        console.log("no more tables");
+    }
+
+    console.log(table_objects);
+
 }
 
 function paintgrid(rows,cols){
@@ -223,11 +253,30 @@ function new_table_button(){
     element.id = "table" + alphabet[1 + newtables];
     element.addEventListener("click", make_interactive.bind(null,"table", "table" + (alphabet[1 + newtables])))
     parent.appendChild(element);
-    newtables++;
+    
 
+    var selectParent = document.getElementById("tables");
+    var option = document.createElement("option");
+    option.value = newtables+1;
+    option.innerHTML = "table" + (alphabet[1 + newtables]);
+    selectParent.appendChild(option);
+
+    var emptyTable = {
+        id: (alphabet[1 + newtables]),
+        capacity: Math.floor(Math.random() * 10)
+    }
+
+    newtables++;
+    //console.log(emptyTable);
+    selectParent.selectedIndex = newtables;
+    
+    table_objects.push(emptyTable);
+    set_capacity(newtables);
+    //console.log(table_objects);
 }
 
 function save_table(){
+    update_current_capacity();
     //console.log(newtables);
     console.log(table_coords);
     var jarray = JSON.stringify(table_coords);
@@ -241,4 +290,64 @@ function save_table(){
     j.open('POST', '../php_pages/tablegrid.php');
     j.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     j.send("number=" + (newtables) + "&" + "tcoords=" + jarray);
+}
+
+function update_current_capacity(){
+
+    var jarray = JSON.stringify(table_objects);
+    //console.log(table_objects);
+    //console.log(jarray);
+    var j = new XMLHttpRequest();
+        j.onreadystatechange = function () {
+            if (j.readyState == 4 && j.status == 200) {
+                console.log(j.responseText);
+                
+            }
+    };
+    j.open('POST', '../php_pages/add_table.php');
+    j.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    j.send("tables=" + jarray);
+
+}
+
+
+function set_capacity(idx){
+    //console.log(idx);
+    document.getElementById("table_capacity").value = table_objects[idx].capacity;
+
+}
+
+function get_all_tables(){
+    
+    var j = new XMLHttpRequest();
+    j.onreadystatechange = function () {
+        if (j.readyState == 4 && j.status == 200) {
+            var table = JSON.parse(j.responseText);
+            //console.log(j.responseText);
+            table_objects = table;
+            for(var i =0;i<table_objects.length;i++){
+                table_objects[i].capacity = parseInt(table_objects[i].capacity); 
+            }
+            console.log(table_objects);
+            set_capacity(0);
+            add_tables_to_dropdown();
+        }
+    };
+    j.open('POST', '../php_pages/get_table.php');
+    j.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    j.send("tableid=" + 1 + "&" + "test=" + 0);
+}
+
+function add_tables_to_dropdown(){
+    const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+    console.log(newtables);
+    var selectParent = document.getElementById("tables");
+    for (let index = 0; index < newtables; index++) {
+        var option = document.createElement("option");
+        option.value = index + 1;
+        option.innerHTML = "table" + (alphabet[1 + index]);
+        selectParent.appendChild(option);
+            
+    }
 }
