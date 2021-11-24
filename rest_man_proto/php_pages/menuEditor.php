81cd306662e044cab5266f346334b73bb350bfe7
Michael Nugent <NugentMichael@live.com>
2021-11-23 23:53:59 -0800

    authorization check on menuEditor script

--- rest_man_proto/php_pages/menuEditor.php
+++ rest_man_proto/php_pages/menuEditor.php
@@ -2,10 +2,37 @@
 
 require 'mysqlConnection.php';
 
+session_start();
+
+//check client is logged in as a user and that user has sufficient privileges
+$username = isset($_SESSION['username'])?$_SESSION['username']:false;
+
+if($username){
+    //check privilege level
+    $query = "SELECT level FROM user_info WHERE username='" . $username . "';";
+    if($result = $conn->query($query)){
+        $privLvl = $result->fetch_object()->level;
+        if($privLvl!=0){
+            return;
+        }
+    }
+    else{
+        return;
+    }
+}
+else{
+    return;
+}
+
 
 
 $categoryUpdate = isset($_POST['categoryUpdate'])?$_POST['categoryUpdate']:false;
 $subcategoryUpdate = isset($_POST['subcategoryUpdate'])?$_POST['subcategoryUpdate']:'{"items":[{"id":"59","newName":"Togarashi Chips with Toasted Sesame Onion Dip","newDescr":"Flash fried Idaho potatoes, dusted with salt and chili-centric Japanese togarashi spice mix. Paired with mayo-based dip made with slow roasted Spanish onion, toasted sesame oil, crispy garlic, and Canadian steak seasoning.","newPrice":"17.00"},{"id":"60","newName":"Clarified Butter Fried Chips and Caviar","newDescr":"Yukon gold potato chips fried in clarified butter served with whipped créme frâiche deced out with chives – plus Ossetra caviar.","newPrice":"18.00"},{"id":"61","newName":"Deviled Crab Dip with Old Bay Chips","newDescr":"Crab, cream cheese, aged white cheddar, yellow cheddar, and cayenne covered in breadcrumbs. Old Bay spiced potato chips with pickled vegetables: chilies, onions, okra, cauliflower, and zucchini.","newPrice":"23.00"}],"newName":"Chips & Dips","id":"9"}';
+
+
+
+
+
 
 //determine which menu update is requested: main category, or subcategory with items
 if($categoryUpdate){
