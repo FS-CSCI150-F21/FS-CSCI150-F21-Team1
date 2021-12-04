75adf79b2719c838b91dcfe9b584da8427be28b6
Michael Nugent <nugentmichael@live.com>
2021-12-03 23:40:58 -0800

    moved sql scripts to folder & edited use-case diagram

--- rest_man_proto/sql/mysqlUserCreation.sql
+++
@@ -1,9 +0,0 @@
--- statements for user creation and privileges granted are not included in automatic database exports
--- Therefore, this script will serve as user creation.
-
--- create user
-CREATE USER IF NOT EXISTS 'rest_manager'@'localhost' IDENTIFIED BY 'iF2ONNbmcCTcdjrd';
-
--- grant privileges to newly created user.  
--- this statement likely to fail if not ran from root.
-GRANT ALL ON rest_info.* to 'rest_manager'@'localhost';
